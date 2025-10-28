0b688194a772aee1cd5ef48b31464bdd4900514f
HSteeleLTS <henry.steele@tufts.edu>
2025-10-28 14:46:23 -0400

    componentizing 541

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.barnes_and_noble import barnes_and_noble_blueprint
 from app.barnes_and_noble import barnes_and_noble_auth_blueprint
 from app.p_and_e_rollup_match import p_and_e_blueprint
 from app.bib_2_holdings_541 import blueprint_541
-from app.bib_2_holdings_541 import blueprint_auth_541
+from app.bib_2_holdings_541 import bib_2_holdings_541_auth_blueprint
 from dotenv import load_dotenv
 from app.gift_fund_bibliography import gift_fund_blueprint
 from app.combine_analytics_subjects_areas import sql_blueprint
@@ -28,7 +28,7 @@ def create_app():
     app.register_blueprint(barnes_and_noble_blueprint, url_prefix="/barnes_and_noble")
     app.register_blueprint(barnes_and_noble_auth_blueprint, url_prefix="/barnes_and_noble_auth")
     app.register_blueprint(blueprint_541, url_prefix="/bib_2_holdings_541")
-    app.register_blueprint(blueprint_auth_541, url_prefex="/auth_541")
+    app.register_blueprint(bib_2_holdings_541_auth_blueprint, url_prefex="/bib_2_holdings_541_auth")
     CORS(app, resources={r"/p_and_e/*": {"origins": "*"}})
     app.register_blueprint(p_and_e_blueprint, url_prefix="/p_and_e")
     app.register_blueprint(gift_fund_blueprint, url_prefix="/gift_fund_bibliography")
